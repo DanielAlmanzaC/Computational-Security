90e6a2814445bbf39fc469787a80e7eced54b245
DanielAlmanzaC <61769799+DanielAlmanzaC@users.noreply.github.com>
2022-03-17 19:23:47 -0600

    Update Practica1_Atbash.py
    This is the atbash method.

--- Practica1_Atbash.py
+++ Practica1_Atbash.py
@@ -1,11 +1,11 @@
-mensaje = input("Escribe tu mensaje\n")
+message = input("Write your message\n")
 
 original = "abcdefghijklmnopqrstuvwxyz"
-clave = "ZYXVUTSRQPONMLKJIHGFEDCBA"
-cifrado = ""
+key = "ZYXVUTSRQPONMLKJIHGFEDCBA"
+encrypted = ""
 
-for i in mensaje.lower():
+for i in message.lower():
     if(i in original):
-        cifrado += clave[original.index(i)]
+        encrypted += key[original.index(i)]
 
-print("Tu mensaje ha sido cifrado: "+cifrado)
+print("Tu mensaje ha sido cifrado: "+encrypted)
